c66cc2b12efa555a31ccfdd550c886794a3334bf
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2017-01-25 18:13:39 -0500

    Whatever

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125000820) do
+ActiveRecord::Schema.define(version: 20170125010455) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170125000820) do
     t.string   "author"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "models", force: :cascade do |t|
